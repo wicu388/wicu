7c3c6c33227f7bd4d9dfd81a4025da589ddd04ba
wicu388 <wistonwc@hotmail.com>
2025-12-10 08:08:52 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{fontFamily:"sans-serif", padding:"40px"}}>
+      <h1>WICU – Tienda IA 24/7</h1>
+      <p>Proyecto base listo para desplegar en Vercel.</p>
+    </div>
+  );
+}
